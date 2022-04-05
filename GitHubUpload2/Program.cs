173ab0c9b3b047eba51d104a02c1ee10be13f3c3
pranavlexisnexis <FNUPra01@risk.regn.net>
2022-04-05 00:17:14 -0500

    initializing repository

---
+++ GitHubUpload2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubUpload2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("GitHub Upload test project");
+        }
+    }
+}
